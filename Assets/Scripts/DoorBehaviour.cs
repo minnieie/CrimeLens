29472a292572574cf5308d82490d7b33035a02f6
minnieie <s10265787@connect.np.edu.sg>
2025-08-08 16:09:32 +0800

    did some lights and added npcs in the lobby, made lobby door functional when player enters trigger

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -7,10 +7,13 @@ public class DoorBehaviour : MonoBehaviour
     public Vector3 doorARotation = new Vector3(0f, -90f, 0f); // Move A left
     public Vector3 doorBRotation = new Vector3(0f, 90f, 0f);  // Move B right
 
+    public bool requiresKeyPress = false; // Toggle this in Inspector
+
     private Vector3 doorAClosedRot, doorAOpenRot;
     private Vector3 doorBClosedRot, doorBOpenRot;
 
     private bool isOpen = false;
+    private bool playerInRange = false;
 
     void Start()
     {
@@ -19,6 +22,40 @@ public class DoorBehaviour : MonoBehaviour
 
         doorAOpenRot = doorAClosedRot + doorARotation;
         doorBOpenRot = doorBClosedRot + doorBRotation;
+    }
+
+    void Update()
+    {
+        if (requiresKeyPress && playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleDoor();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+
+            if (!requiresKeyPress)
+            {
+                OpenDoors(); // Auto-open
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+
+            if (!requiresKeyPress)
+            {
+                CloseDoors(); // Auto-close
+            }
+        }
     }
 
     public void ToggleDoor()
@@ -40,7 +77,7 @@ public class DoorBehaviour : MonoBehaviour
         doorB.rotation = Quaternion.Euler(doorBOpenRot);
         isOpen = true;
     }
-    
+
     public void CloseDoors()
     {
         doorA.rotation = Quaternion.Euler(doorAClosedRot);
